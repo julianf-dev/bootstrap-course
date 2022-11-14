e298b6f3fa7c732e654a8195f4543fe048000617
JulianF <juliandavidbf@gmail.com>
2022-11-14 09:30:25 -0500

    @feat: Create footer and header

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import "../scss/styles.scss";
+
+import Alert from "bootstrap/js/dist/alert";
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from "bootstrap";
